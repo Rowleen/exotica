f7f47fbd2f79736e64fbac6d55bff3a56fb70915
Rowleen <julianpeacevedo@gmail.com>
2024-06-27 18:25:42 +0200

    Add searcher and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import { Header } from './components';
+'use client';
+import { Header, Searcher } from './components';
 
 import styles from './sass/home.module.sass';
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <main className={styles.home}>
       <Header />
-    </div>
+
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.title}>The places you dream of</h1>
+
+        <h2 className={styles.subtitle}>Let's live new adventures</h2>
+      </div>
+
+      <Searcher />
+    </main>
   );
 }
