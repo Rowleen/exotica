f5323c7f2f1ef92eba40871ad58d3b631d219f3e
Rowleen <julianpeacevedo@gmail.com>
2024-06-28 23:31:46 +0200

    Create hook to filter trips

---
+++ app/Hooks/useFilterTrips.ts
@@ -0,0 +1,51 @@
+import { FormEvent, useEffect, useState } from 'react';
+import type { Trip } from '../core/domain/entities/Trip';
+import useGetTrips from '../core/domain/useCases/useGetTrips';
+
+const useFilterTrips = () => {
+  const { trips: tripsToFilter, isLoading } = useGetTrips();
+
+  const [trips, setFilteredTrips] = useState<Trip[]>([]);
+
+  useEffect(() => {
+    if (tripsToFilter) {
+      setFilteredTrips(tripsToFilter);
+    }
+  }, [tripsToFilter, isLoading]);
+
+  const handleFilterByStatus = (event: React.MouseEvent) => {
+    const { value } = <HTMLTextAreaElement>event.target;
+
+    if (value === 'all' && !isLoading && tripsToFilter) {
+      setFilteredTrips(tripsToFilter);
+    }
+    if (value === 'toDo' && !isLoading && tripsToFilter) {
+      const toDoTrips = tripsToFilter?.filter((trip) => trip.status === 'todo');
+      setFilteredTrips(toDoTrips);
+    }
+    if (value === 'done' && !isLoading && tripsToFilter) {
+      const doneTrips = tripsToFilter?.filter((trip) => trip.status === 'done');
+      setFilteredTrips(doneTrips);
+    }
+  };
+
+  const handleFilterByTerm = (
+    searchTerm: string,
+    event: FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+
+    if (searchTerm && tripsToFilter && !isLoading) {
+      const regex = new RegExp(searchTerm, 'i');
+
+      const resultTrips = tripsToFilter?.filter(
+        (trip) => regex.test(trip.title) || regex.test(trip.description)
+      );
+      setFilteredTrips(resultTrips);
+    }
+  };
+
+  return { trips, isLoading, handleFilterByStatus, handleFilterByTerm };
+};
+
+export default useFilterTrips;
