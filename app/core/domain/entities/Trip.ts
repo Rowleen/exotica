cae040e25e69a9e964400f15f7a81214ebe11978
Rowleen <julianpeacevedo@gmail.com>
2024-07-01 03:53:03 +0200

    Change id as optional

--- app/core/domain/entities/Trip.ts
+++ app/core/domain/entities/Trip.ts
@@ -1,10 +1,10 @@
-import { Itinerary } from './Itinerary';
+import { Itinerary } from './Itinerary'
 
 export type Trip = {
-  readonly id: number;
-  title: string;
-  description: string;
-  photo_url: string;
-  status: string;
-  itinerary: Itinerary[];
-};
+  readonly id?: number
+  title: string
+  description: string
+  photo_url: string
+  status: string
+  itinerary: Itinerary[]
+}
