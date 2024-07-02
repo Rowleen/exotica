05e1f8c1b8634788d5c29470ecb3acac57d3034f
Rowleen <julianpeacevedo@gmail.com>
2024-07-02 04:32:19 +0200

    Avoid arrow functions on onChange events

--- app/components/Searcher/Searcher.tsx
+++ app/components/Searcher/Searcher.tsx
@@ -1,37 +1,44 @@
-import { FC, FormEvent, useState } from 'react';
-import Button from '../Button/Button';
+import { FC, FormEvent, useState } from 'react'
+import Button from '../Button/Button'
 
-import styles from './searcher.module.sass';
+import type { onChangeEvent } from '../../shared/types/onChangeEvent'
+
+import styles from './searcher.module.sass'
 
 interface SearcherProps {
-  handleOnSearch: (searchTerm: string, e: FormEvent<HTMLFormElement>) => void;
+  handleOnSearch: (searchTerm: string, e: FormEvent<HTMLFormElement>) => void
 }
 
 const Searcher: FC<SearcherProps> = ({ handleOnSearch }) => {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>('')
+
+  const handleOnChange = (event: onChangeEvent) =>
+    setSearchTerm(event.target.value)
+
+  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    handleOnSearch(searchTerm, event)
+  }
 
   return (
-    <form
-      className={styles.searchForm}
-      onSubmit={(e) => handleOnSearch(searchTerm, e)}
-    >
+    <form className={styles.searchForm} onSubmit={handleSearch}>
       <input
         className={styles.input}
-        placeholder="Search trips"
-        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder='Search trips'
+        onChange={handleOnChange}
       />
 
       <div className={styles.buttonWrapper}>
         <Button
-          text="Search"
-          type="submit"
-          shape="button"
-          color="primary"
-          size="small"
+          text='Search'
+          type='submit'
+          shape='button'
+          color='primary'
+          size='small'
         />
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default Searcher;
+export default Searcher
