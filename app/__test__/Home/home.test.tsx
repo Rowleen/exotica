7cf99be2647c027caade419210a697a290a80cb5
Rowleen <julianpeacevedo@gmail.com>
2024-07-02 03:08:04 +0200

    Create home test

--- app/__test__/Home/home.test.tsx
+++ app/__test__/Home/home.test.tsx
@@ -1,29 +1,48 @@
+import React from 'react'
 import '@testing-library/jest-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Home from '../../page'
 
 const queryClient = new QueryClient()
 
 describe('Home', () => {
+  const renderComponent = (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  )
+
   it('renders homepage unchanged', () => {
-    const { container } = render(
-      <QueryClientProvider client={queryClient}>
-        <Home />
-      </QueryClientProvider>
-    )
+    const { container } = render(renderComponent)
     expect(container).toMatchSnapshot()
   })
 
-  it('renders a heading', () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Home />
-      </QueryClientProvider>
-    )
+  it('renders the title and subtitle of the app', () => {
+    const { getByRole } = render(renderComponent)
 
-    const heading = screen.getByRole('heading', { level: 1 })
+    const heading = getByRole('heading', { level: 1 })
+    const subHeading = getByRole('heading', { level: 2 })
 
     expect(heading).toBeInTheDocument()
+    expect(subHeading).toBeInTheDocument()
+  })
+
+  it('render the pills of sorting trips list', () => {
+    const { getByRole } = render(renderComponent)
+
+    const pillAll = getByRole('button', { name: 'All' })
+    const pillUpComing = getByRole('button', { name: 'Upcoming' })
+    const pillComplete = getByRole('button', { name: 'Complete' })
+
+    expect(pillAll).toBeInTheDocument()
+    expect(pillUpComing).toBeInTheDocument()
+    expect(pillComplete).toBeInTheDocument()
+  })
+
+  it('render create trip modal', () => {
+    const initialState = true
+
+    React.useState = jest.fn().mockReturnValue([initialState, {}])
   })
 })
